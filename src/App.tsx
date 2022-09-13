4fe802920c94dfe5a89d525a385c71c046602879
erik2501 <eriksw2501@gmail.com>
2022-09-13 13:09:31 +0200

    Deleted files and added components

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import Header from './Components/Header';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Hello world</h1>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          hello world
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container1'>
+      <Header></Header>
     </div>
   );
 }
 
 export default App;
+
+
