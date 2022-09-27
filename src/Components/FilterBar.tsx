c34d7ad30b51b8f4ff77768be0f3851c96c203ac
erik2501 <eriksw2501@gmail.com>
2022-09-27 15:19:23 +0200

    Only filtered cards display

--- src/Components/FilterBar.tsx
+++ src/Components/FilterBar.tsx
@@ -1,9 +1,7 @@
 import { Stack } from '@mui/material';
 import { useEffect, useState } from 'react';
 import { Commit } from '../helpers/types';
-import CommitCard from './CommitCard';
 import { getCommits } from '../helpers/fetches';
-import Box from '@mui/material/Box';
 import InputLabel from '@mui/material/InputLabel';
 import MenuItem from '@mui/material/MenuItem';
 import FormControl from '@mui/material/FormControl';
@@ -13,26 +11,27 @@ import dayjs, { Dayjs } from 'dayjs';
 import TextField from '@mui/material/TextField';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import Button from '@mui/material/Button';
-import { styled } from '@mui/system';
 
-function FilterBar() {
+function FilterBar({filteredCommits , setFilteredCommits} : {filteredCommits : any, setFilteredCommits : any}) {
 
     const [commits, setCommits] = useState<Commit[] | undefined>([])
     const [filtered, setFiltered] = useState<Commit[] | undefined>([])
-    const [namefilter, setNameFilter] = useState<Commit[] | undefined>([])
     const [searchName, setSearchName] = useState<String | undefined>()
-    const [startDate, setStartDate] = useState<Dayjs | null>(dayjs())
-    const [endDate, setEndDate] = useState<Dayjs | null>(dayjs())
+    const [startDate, setStartDate] = useState<Dayjs | null>()
+    const [endDate, setEndDate] = useState<Dayjs | null>()
     const names = Array.from(new Set(commits?.map(x => x.author_name)))
+    /*
+    Utgåtte funksjoner, mulig slettes
     const [dateFilter, setDateFilter] = useState<Commit[] | undefined>([])
-
-
+    const [namefilter, setNameFilter] = useState<Commit[] | undefined>([])
+    */
     useEffect(() => {
         getCommits().then(data => setCommits(data))
+        getCommits().then(data => setFilteredCommits(data))
     }, [])
 
 
-    //filtrerer alfabetisk skal kanskje ikke brukes
+    //filtrerer alfabetisk, skal kanskje ikke brukes
     const filterAlpha = () => {
         if (commits != undefined) {
             setFiltered(commits?.sort((a, b) => a.author_name.localeCompare(b.author_name)));
@@ -41,15 +40,17 @@ function FilterBar() {
 
     //klarer alle filtere
     const clearFilters = () => {
-        getCommits().then(data => setFiltered(data))
+        getCommits().then(data => setFilteredCommits(data))
         setStartDate(dayjs())
         setEndDate(dayjs())
-        setSearchName(undefined)
+        setSearchName(undefined) //funker ikke, spør stud ass
     }
+    
     //filtrerer på navn
     const filterName = () => {
         setFiltered(commits?.filter(x => x.author_name === searchName));
-        console.log(namefilter);
+        console.log(searchName);
+        console.log(filtered);
     }
     
     //clear kun liste
@@ -61,7 +62,6 @@ function FilterBar() {
     const filterDate = () => {
 
         if (startDate != null && endDate != null){
-            console.log(startDate <= endDate);
             setFiltered(commits?.filter(a => (dayjs(a.committed_date) > startDate && dayjs(a.committed_date) < endDate)));
             console.log(filtered)
         }
@@ -70,6 +70,7 @@ function FilterBar() {
     const submitFilter = () => {
         filterName();
         filterDate();
+        setFilteredCommits(filtered); //fungerer men fortsatt på to forsøk
     }
 
     const changeFilterName = (e: any) => setSearchName(e.target.value)
