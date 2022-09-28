1d21ee5b47afc527070cc9d5d1a05363b50ae3f8
erik2501 <eriksw2501@gmail.com>
2022-09-28 11:53:02 +0200

    Added mobile drawer for filter

--- src/Components/FilterBar.tsx
+++ src/Components/FilterBar.tsx
@@ -1,9 +1,10 @@
-import { Stack } from '@mui/material';
+import { Drawer, IconButton, ListItem, ListItemIcon, Stack, Typography, useMediaQuery, useTheme } from '@mui/material';
 import { useEffect, useState } from 'react';
 import { Commit } from '../helpers/types';
 import { getCommits } from '../helpers/fetches';
 import InputLabel from '@mui/material/InputLabel';
 import MenuItem from '@mui/material/MenuItem';
+import FilterList from '@mui/icons-material/FilterList';
 import FormControl from '@mui/material/FormControl';
 import Select from '@mui/material/Select';
 import { DesktopDatePicker, LocalizationProvider } from '@mui/x-date-pickers';
@@ -11,6 +12,8 @@ import dayjs, { Dayjs } from 'dayjs';
 import TextField from '@mui/material/TextField';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import Button from '@mui/material/Button';
+import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
+import { Container } from '@mui/system';
 
 function FilterBar({filteredCommits , setFilteredCommits} : {filteredCommits : any, setFilteredCommits : any}) {
 
@@ -20,6 +23,10 @@ function FilterBar({filteredCommits , setFilteredCommits} : {filteredCommits : a
     const [startDate, setStartDate] = useState<Dayjs | null>()
     const [endDate, setEndDate] = useState<Dayjs | null>()
     const names = Array.from(new Set(commits?.map(x => x.author_name)))
+    const [openDrawer, setOpenDrawer] = useState<boolean>(false)
+    const theme = useTheme();
+    const matches = useMediaQuery(theme.breakpoints.down('sm'));
+    console.log(matches);
     /*
     Utgåtte funksjoner, mulig slettes
     const [dateFilter, setDateFilter] = useState<Commit[] | undefined>([])
@@ -72,14 +79,60 @@ function FilterBar({filteredCommits , setFilteredCommits} : {filteredCommits : a
         filterDate();
         setFilteredCommits(filtered); //fungerer men fortsatt på to forsøk
     }
-
+    const toggleOpenDrawer = () => setOpenDrawer(!openDrawer)
     const changeFilterName = (e: any) => setSearchName(e.target.value)
     const handleChangeStartDate = (newValue: Dayjs | null) => setStartDate(newValue)
     const handleChangeEndDate = (newValue: Dayjs | null) => setEndDate(newValue)
 
     //Det som vises på skjerm
     return (
-        <Stack spacing={2} direction='row' sx={{ p: 2 }}>
+        <>
+        {matches ? 
+            <Stack>
+                <Container sx={{width:'190px'}}>
+                    <IconButton onClick={toggleOpenDrawer} sx={{ width:'100%'}}>
+                        <FilterList/>
+                    </IconButton>
+                </Container>
+                <Drawer open={openDrawer} anchor='right'>
+                    <Stack spacing={2} sx={{ p: 2 }}>
+                        <IconButton onClick={toggleOpenDrawer}>
+                            <ArrowBackIosIcon/>
+                        </IconButton>
+                        <FormControl sx={{ width: 130 }}>
+                            <InputLabel> Navn </InputLabel>
+                            <Select onChange={changeFilterName}>
+                                {names?.map((name) => (
+                                    <MenuItem value={name}>
+                                        {name}
+                                    </MenuItem>
+                                ))}
+                            </Select>
+                        </FormControl>
+
+                        <LocalizationProvider dateAdapter={AdapterDayjs}>
+                            <DesktopDatePicker
+                                label="Start Date"
+                                inputFormat="MM/DD/YYYY"
+                                value={startDate}
+                                onChange={handleChangeStartDate}
+                                renderInput={(params) => <TextField {...params} />}
+                            />
+                            <DesktopDatePicker
+                                label="End Date"
+                                inputFormat="MM/DD/YYYY"
+                                value={endDate}
+                                onChange={handleChangeEndDate}
+                                renderInput={(params) => <TextField {...params} />}
+                            />
+                        </LocalizationProvider>
+                        <Button variant="contained" onClick={submitFilter} color="success">Submit</Button>
+                        <Button variant="contained" onClick={clearFilters} color='error'>Clear</Button>
+                    </Stack>
+                </Drawer>
+        </Stack>
+ :
+            <Stack spacing={2} direction='row' sx={{ p: 2 }}>
             <FormControl sx={{ width: 130 }}>
                 <InputLabel> Navn </InputLabel>
                 <Select onChange={changeFilterName}>
@@ -90,7 +143,7 @@ function FilterBar({filteredCommits , setFilteredCommits} : {filteredCommits : a
                     ))}
                 </Select>
             </FormControl>
-
+    
             <LocalizationProvider dateAdapter={AdapterDayjs}>
                 <DesktopDatePicker
                     label="Start Date"
@@ -110,7 +163,8 @@ function FilterBar({filteredCommits , setFilteredCommits} : {filteredCommits : a
             <Button variant="contained" onClick={submitFilter} color="success">Submit</Button>
             <Button variant="contained" onClick={clearFilters} color='error'>Clear</Button>
         </Stack>
-
+    }   
+        </>
     )
 }
 
