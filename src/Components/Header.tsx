0ca51357a29deaa320bbde4eb328884af3e109bf
erik2501 <eriksw2501@gmail.com>
2022-09-29 15:19:42 +0200

    New header with css

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 
 const Header = () => {
+
     return (
-        <div style={{ width: '100%', height: '60px', backgroundColor: 'lightblue', display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
-            <h2 style={{margin: '0px'}}>Project 2</h2>
+        <div className='header'>
+            <h2>Project 2</h2>
         </div>
     )
 };
