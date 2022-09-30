a8850dbc41b55afecbe6f6b3d99c0d7f4c00fe4a
Catarina Skibsrud <catarins@stud.ntnu.no>
2022-09-30 14:37:12 +0200

    fixed date on cards

--- src/Components/IssueCard.tsx
+++ src/Components/IssueCard.tsx
@@ -6,7 +6,10 @@ function IssueCard({ issue }: { issue: Issue }) {
     return (
         <div className='commitCard'>
             <h5>state: {issue.state}</h5>
-            <h5>{issue.closed_by ? "closed at: " + issue.closed_at : "created at: " + issue.created_at}</h5>
+            <h5>{
+                issue.closed_by ?
+                    "closed at: " + (new Date(issue.closed_at)).toLocaleDateString('en-GB') :
+                    "created at: " + (new Date(issue.created_at)).toLocaleDateString('en-GB')}</h5>
             <h2>{issue.title}</h2>
             <h5>{issue.closed_by ? "closed by: " + issue.closed_by.name : "created by: " + issue.author.name} </h5>
             <h4>{issue.description}</h4>
